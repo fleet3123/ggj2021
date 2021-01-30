8a0a9234b4797b40b7bffa38a75d186f364200bf
DanielSpaten <78225081+DanielSpaten@users.noreply.github.com>
2021-01-30 16:52:51 +0100

    Add files via upload

---
+++ Scripts/Enemy3.gd
@@ -0,0 +1,83 @@
+extends KinematicBody2D
+
+export (PackedScene) var Bullet2
+
+var speed = 100
+
+var target = null
+var cooldown = false
+onready var cooldownTimer = $cooldownTimer
+
+var state = IDLE
+
+
+enum {
+	IDLE,
+	CHASE,
+	ATTACK
+}
+
+
+
+func _physics_process(delta):
+	match state:
+		IDLE:
+			seek_player()
+		CHASE:
+			chase_state()
+		ATTACK:
+			attack_state()
+			
+			
+func seek_player():
+	if target != null:
+		state = CHASE
+		
+func chase_state():
+	if target == null:
+		state = IDLE
+		return
+	
+		
+	if $Attack.overlaps_body(target) and !cooldown:
+		state = ATTACK
+
+	var direction = (target.global_position - global_position).normalized()
+		
+	if $too_close.overlaps_body(target):
+		move_and_slide((direction*(-1)) * (speed/2))
+		
+	elif !$Attack.overlaps_body(target):
+		move_and_slide(direction*speed)
+
+func attack_state():
+	shoot()
+	state = IDLE
+	
+func shoot():
+	var b = Bullet2.instance()
+	b.set_direction((target.global_position - global_position).normalized())
+	b.shoot()
+	b.set_position(global_position)
+	b.set_target(target)
+	owner.add_child(b)
+	
+	cooldown = true
+	cooldownTimer.start(2)
+	
+func recover_state():
+	pass
+
+
+func _on_cooldownTimer_timeout():
+	cooldownTimer.stop()
+	cooldown = false
+
+
+func _on_Attack_body_entered(body):
+	if !cooldown:
+		state = ATTACK
+
+
+func _on_Detection_body_entered(body):
+	target = body
