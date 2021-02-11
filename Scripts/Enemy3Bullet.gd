349afd9ec2ea196292f9294a213e0ad17d3c5875
Lorenz Wech <lorenz.wech@web.de>
2021-02-11 05:54:03 +0100

    Fixed bullet vanishing in wall on shoot
    Bullet now hits wall depending on the angle by using dot product.
    Direction is found via group tag of walls

--- Scripts/Enemy3Bullet.gd
+++ Scripts/Enemy3Bullet.gd
@@ -31,4 +31,24 @@ func _physics_process(delta):
 func _on_Enemy3Bullet_body_entered(body):
     if body == target:
         target.take_damage(damage)
-    queue_free()
+        queue_free()
+    elif body.is_in_group ( "wall" ):
+        entered_wall ( body )
+    else:
+        print ( "hit", body )
+        
+func entered_wall ( wall : Node2D ):
+    
+    var collision_test = Vector2.ZERO
+    if ( wall.is_in_group ( "North" ) ):
+        collision_test = Vector2.DOWN
+    elif ( wall.is_in_group ( "East" ) ):
+        collision_test = Vector2.LEFT
+    elif ( wall.is_in_group ( "South" ) ):
+        collision_test = Vector2.UP
+    elif ( wall.is_in_group ( "West" ) ):
+        collision_test = Vector2.RIGHT
+    
+    var dot = collision_test.dot ( transform.x )
+    if dot <= 0 :
+        queue_free()
