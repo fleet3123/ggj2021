f335b4ed497554d460d70895bfa6cab8f4d3f06e
Lorenz Wech <lorenz.wech@web.de>
2021-02-22 21:01:41 +0100

    Bullet script uses onready vars and deferred call.
    This fixes an annoying warning log.

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -4,6 +4,8 @@ extends Area2D
 var speed : int = 750
 var damage : int = 1
 
+onready var collision = $CollisionShape2D
+onready var animation = $AnimationPlayer
 func _physics_process ( delta ):
     position += transform.x * speed * delta
 
@@ -15,9 +17,8 @@ func _on_Bullet_body_entered ( body ):
 
 func die ():
     speed = 0
-    $CollisionShape2D.disabled = true
-    $AnimationPlayer.play ( "Hit" )
-    
+    collision.call_deferred ( "set", "disabled", true )
+    animation.play ( "Hit" )
     
 func _on_AnimationPlayer_animation_finished(anim_name):
     if anim_name == "Hit":
