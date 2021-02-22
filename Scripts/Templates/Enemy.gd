fe72bd893112a5a7cc3d90c81cd9a3384739ba47
Lorenz Wech <lorenz.wech@web.de>
2021-02-22 20:45:48 +0100

    Added template scene for enemies

---
+++ Scripts/Templates/Enemy.gd
@@ -0,0 +1,48 @@
+class_name Enemy
+extends Node2D
+
+
+# signal spawned
+signal hit
+signal died
+
+export ( int ) var starting_hp = 1
+export ( int ) var xp_to_give = 0
+export ( Script ) var behavior
+
+var target : Object = null
+var curr_hp : int = starting_hp
+
+onready var anim = $AnimationPlayer
+
+func _init():
+    pass
+
+func _ready ():
+    curr_hp = starting_hp
+    print ( "Enemy ready", self, anim)
+    # emit_signal ( "spawned" )
+
+func take_damage ( dmg : int ):
+    emit_signal ( "hit" )
+    
+    curr_hp -= dmg
+    if curr_hp <= 0:
+        die()
+
+func die ():
+    emit_signal ( "died" )
+    if target:
+        target.give_xp ( xp_to_give )
+    
+    if anim.has_animation ( "Die" ):
+        anim.play ( "Die" )
+    else:
+        queue_free();
+
+func _on_AnimationPlayer_animation_finished ( anim_name : String ):
+    if anim_name == "Die":
+        queue_free()
+    
+    
+
