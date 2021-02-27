c6dcce821b174a694d2dce5d479eec806a1a9819
Lorenz Wech <lorenz.wech@web.de>
2021-02-27 15:05:38 +0100

    moved room spawning from exit to room template
    This fixed a nasty error message.

--- Scripts/Templates/Exit.gd
+++ Scripts/Templates/Exit.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal exit_entered ( body, direction )
+
 enum Direction{
     NORTH,
     EAST,
@@ -42,50 +44,4 @@ func _ready ():
     camera = get_parent().get_node ( get_parent().camera_path ) as Camera2D
 
 func _on_Exit_body_entered ( body ):
-
-    var parent = get_parent()
-    if body.is_in_group ( "player" ) and body.in_control == true:
-#            get_tree().reload_current_scene()
-        body.in_control = false
-        var room = null
-        if next_room_path:
-            room = parent.get_node ( next_room_path )
-        else:
-            room = next_room.instance()
-            room.position = parent.get_node ( ROOMSPAWN + ENUMTOSTRING [ direction ] ).global_position
-            room.player_path = parent.player_path
-            room.camera_path = parent.camera_path
-            
-            var parent_of_parent = parent.get_parent()
-            parent_of_parent.add_child ( room )
-            parent.set_exit_room_path ( direction, room.get_path() )
-            room.set_exit_room_path ( OPPOSITE[direction], parent.get_path() )
-
-        tween.interpolate_property (
-            camera
-            , "position"
-            , camera.position
-            , room.get_node ( CAMERAPOINT ).global_position
-            , 1
-            , Tween.TRANS_QUAD , Tween.EASE_IN_OUT
-            )
-        tween.interpolate_property (
-            body
-            , "position"
-            , body.position
-            , room.get_node ( PLAYERSTART + PLAYERDICT [ direction ] ).global_position
-            , 1
-            , Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
-            )
-#        camera.position = room.get_node ( CAMERAPOINT ).global_position
-#        player.position = room.get_node ( PLAYERSTART + PLAYERDICT [ direction ] ).global_position
-#        player.set_exit_collision ( false )
-        tween.start()
-        room.emit_signal ( "room_entered" )
-
-func _on_Tween_tween_completed(object, key):
-    print (object, " ", key)
-    if object.is_in_group("player"):
-        print ( "is player" )
-        object.in_control = true
-#        player.set_exit_collision ( true )
+    emit_signal ( "exit_entered", body, direction )
--- Scripts/Templates/Room.gd
+++ Scripts/Templates/Room.gd
@@ -51,6 +51,10 @@ export (NodePath) var room_path_east
 export (NodePath) var room_path_south
 export (NodePath) var room_path_west
 
+var _camera : Camera2D = null
+var _exits : Dictionary = {}
+
+onready var tween = $Tween
 
 onready var exit_north = $Exit_North
 onready var exit_east = $Exit_East
@@ -58,6 +62,12 @@ onready var exit_south = $Exit_South
 onready var exit_west = $Exit_West
 
 func _ready():
+    _camera = get_node ( camera_path )
+    _exits [ Direction.NORTH ] = [ room_path_north, room_north ]
+    _exits [ Direction.EAST ] = [ room_path_east, room_east ]
+    _exits [ Direction.SOUTH ] = [ room_path_south, room_south ]
+    _exits [ Direction.WEST ] = [ room_path_west, room_west ]
+
     exit_north.next_room = room_north
     exit_north.next_room_path = room_path_north
     
@@ -72,6 +82,7 @@ func _ready():
 
 
 func set_exit_room_path ( var direction, var path : NodePath ):
+    _exits [ direction ] [0] = path
     match direction:
         Direction.NORTH:
             room_path_north = path
@@ -84,8 +95,50 @@ func set_exit_room_path ( var direction, var path : NodePath ):
             exit_south.next_room_path = room_path_south
         Direction.WEST:
             room_path_west = path
-            exit_west.next_room_path = room_path_west
+            exit_south.next_room_path = room_path_west
+            
 
+func spawn_room ( direction ) -> Node2D :
+    var next_room = _exits [ direction ][ 1 ]
+    var room = next_room.instance()
+    room.position = get_node ( ROOMSPAWN + ENUMTOSTRING [ direction ] ).global_position
+    room.player_path = player_path
+    room.camera_path = camera_path
+        
+    var parent = get_parent()
+    parent.add_child ( room )
+    set_exit_room_path ( direction, room.get_path() )
+    room.set_exit_room_path ( OPPOSITE [ direction ], get_path() )
+    
+    return room
+
+func enter_room ( player, direction ):
+    var room = null
+    var next_room_path = _exits [ direction ][ 0 ]
+    
+    if next_room_path:
+        room = get_node ( next_room_path )
+    else:
+        room = spawn_room ( direction )
+
+    tween.interpolate_property (
+        _camera
+        , "position"
+        , _camera.position
+        , room.get_node ( CAMERAPOINT ).global_position
+        , 1
+        , Tween.TRANS_QUAD , Tween.EASE_IN_OUT
+        )
+    tween.interpolate_property (
+        player
+        , "position"
+        , player.position
+        , room.get_node ( PLAYERSTART + PLAYERDICT [ direction ] ).global_position
+        , 1
+        , Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
+        )
+    tween.start()
+    room.emit_signal ( "room_entered" )
 
 func set_encounter ( new_encounter ):
     encounter = new_encounter
@@ -98,4 +151,16 @@ func _on_room_entered():
 
 
 func _on_Room_tree_entered():
-    pass # Replace with function body.
+    print ( self, " entered tree.")
+
+
+func _on_exit_entered ( body, direction ):
+    if body.is_in_group ( "player" ) and body.in_control == true:
+        body.in_control = false
+        call_deferred ( "enter_room", body, direction )
+
+
+func _on_Tween_tween_completed ( object, key ):
+    print (object, " ", key)
+    if object.is_in_group("player"):
+        object.in_control = true
