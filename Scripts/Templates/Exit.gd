6bb381a8e8385dd7c96de7781de060e8afc433ef
Lorenz Wech <lorenz.wech@web.de>
2021-03-14 20:44:15 +0100

    clean up code and scenes

--- Scripts/Templates/Exit.gd
+++ Scripts/Templates/Exit.gd
@@ -9,33 +9,7 @@ enum Direction {
     WEST,
 }
 
-# const PLAYERSTART = "PlayerStart_"
-# const ROOMSPAWN = "RoomSpawn_"
-# const CAMERAPOINT = "CameraPoint"
-# const ENUMTOSTRING = {
-#     Direction.NORTH: "North",
-#     Direction.EAST: "East",
-#     Direction.SOUTH: "South",
-#     Direction.WEST: "West"
-#     }
-# const PLAYERDICT = {
-#     Direction.NORTH: "South",
-#     Direction.EAST: "West",
-#     Direction.SOUTH: "North",
-#     Direction.WEST: "East"
-#     }
-# const OPPOSITE = {
-#     Direction.NORTH: Direction.SOUTH,
-#     Direction.EAST: Direction.WEST,
-#     Direction.SOUTH: Direction.NORTH,
-#     Direction.WEST: Direction.EAST
-# }
-
 export (Direction)  var direction
-
-# var next_room : PackedScene = null
-# var next_room_path : NodePath = ""
-#export (PackedScene) var this_room
 
 func _ready ():
     pass
